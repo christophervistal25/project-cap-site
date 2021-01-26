96f2591a99a2c7311b0bf56d3391c1848edb4535
CHRis <christophervistal25@gmail.com>
2021-01-26 20:57:04 +0800

    Establishment add relation for city and barangay

--- app/Establishment.php
+++ app/Establishment.php
@@ -7,4 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 class Establishment extends Model
 {
     protected $fillable = ['name', 'type', 'address', 'contact_no', 'geo_tag_location', 'province', 'city_zip_code', 'barangay_id'];
+
+    public function city()
+    {
+        return $this->belongsTo('App\City');
+    }
+
+    public function barangay()
+    {
+        return $this->belongsTo('App\Barangay');
+    }
+
 }
